a4c114263a9b41904a72fc243b8bee8bfa3f1337
shira-st <shiraishi.t0421@gmail.com>
2022-08-02 01:46:45 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
 # train_model
+
+```python
+import numpy as np
+import tensorflow as tf
+from train_model import train_model
+
+def set_seed(seed):
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    return
+
+
+def create_sample_dataset():
+    x = np.random.random((100, 10))
+    y = np.random.randint(0, 2, (100, 1))
+    x = tf.data.Dataset.from_tensor_slices(x)
+    y = tf.data.Dataset.from_tensor_slices(y)
+    dataset = tf.data.Dataset.zip((x, y)).shuffle(100).batch(10)
+    return dataset
+
+
+def create_sample_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(10)),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics="accuracy")
+    return model
+```
+
+model.fit()
+```python
+seed = 0
+set_seed(seed)
+dataset = create_sample_dataset()
+model = create_sample_model()
+
+epochs = 5
+history = model.fit(dataset, epochs=epochs)
+print(history.history)
+```
+```
+Epoch 1/5
+10/10 [==============================] - 0s 776us/step - loss: 2.1481 - accuracy: 0.5100
+Epoch 2/5
+10/10 [==============================] - 0s 776us/step - loss: 1.9838 - accuracy: 0.4900
+Epoch 3/5
+10/10 [==============================] - 0s 720us/step - loss: 1.8324 - accuracy: 0.4900
+Epoch 4/5
+10/10 [==============================] - 0s 776us/step - loss: 1.8124 - accuracy: 0.4900
+Epoch 5/5
+10/10 [==============================] - 0s 1ms/step - loss: 1.8042 - accuracy: 0.5000
+{'loss': [2.1480836868286133, 1.9837868213653564, 1.8324090242385864, 1.812350869178772, 1.8041961193084717], 'accuracy': [0.5099999904632568, 0.49000000953674316, 0.49000000953674316, 0.49000000953674316, 0.5]}
+```
+
+train_model()
+```python
+seed = 0
+set_seed(seed)
+dataset = create_sample_dataset()
+model = create_sample_model()
+
+epochs = 5
+step_period = 3
+epoch_period = 2
+history = train_model(dataset, model, epochs, step_period=step_period, epoch_period=epoch_period)
+print(history.history)
+```
+```
+Epoch 1/5
+10/10 [==============================] - 0s 30ms/step - loss: 2.1481 - accuracy: 0.5100
+Epoch 2/5
+10/10 [==============================] - 0s 10ms/step - loss: 1.9838 - accuracy: 0.4900
+Epoch 3/5
+10/10 [==============================] - 0s 12ms/step - loss: 1.8324 - accuracy: 0.4900
+Epoch 4/5
+10/10 [==============================] - 0s 16ms/step - loss: 1.8124 - accuracy: 0.4900
+Epoch 5/5
+10/10 [==============================] - 0s 13ms/step - loss: 1.8042 - accuracy: 0.5000
+{'loss': [2.1480836868286133, 1.9837868213653564, 1.8324090242385864, 1.812350869178772, 1.8041961193084717], 'accuracy': [0.5099999904632568, 0.49000000953674316, 0.49000000953674316, 0.49000000953674316, 0.5]}
+```
+
+restore
+```python
+seed = 0
+set_seed(seed)
+dataset = create_sample_dataset()
+model = create_sample_model()
+
+epochs = 5
+initial_step = 3
+initial_epoch = 2
+history = train_model(dataset, model, epochs, initial_step=initial_step, initial_epoch=initial_epoch)
+print(history.history)
+```
+```
+Epoch 3/5
+10/10 [==============================] - 0s 21ms/step - loss: 1.8324 - accuracy: 0.4900
+Epoch 4/5
+10/10 [==============================] - 0s 537us/step - loss: 1.8124 - accuracy: 0.4900
+Epoch 5/5
+10/10 [==============================] - 0s 798us/step - loss: 1.8042 - accuracy: 0.5000
+{'loss': [2.1480836868286133, 1.9837868213653564, 1.8324090242385864, 1.812350869178772, 1.8041961193084717], 'accuracy': [0.5099999904632568, 0.49000000953674316, 0.49000000953674316, 0.49000000953674316, 0.5]}
+```
---
+++ train_model.py
@@ -0,0 +1,158 @@
+import os
+import joblib
+import numpy as np
+import tensorflow as tf
+tf.get_logger().setLevel("ERROR")
+
+
+class CheckpointManager():
+    def __init__(self, ckpt_dir):
+        self.ckpt_path = os.path.join(ckpt_dir, "ckpt_{epoch:04d}_{step:04d}", "{name}")
+
+    # save a dataset
+    def save_iterator(self, iterator, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="iterator")
+        ckpt = tf.train.Checkpoint(iterator)
+        ckpt.write(path)
+        return
+
+    # restore a dataset
+    def restore_iterator(self, iterator, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="iterator")
+        ckpt = tf.train.Checkpoint(iterator)
+        ckpt.read(path).assert_consumed()
+        return
+
+    # save model weights
+    def save_model(self, model, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="model")
+        model.save_weights(path)
+        return
+
+    # restore model weights
+    def restore_model2(self, model, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="model")
+        model.load_weights(path)
+        return
+
+    # save metrics
+    def save_metrics(self, model, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="metrics")
+        ckpt = tf.train.Checkpoint()
+        ckpt.metrics = model.metrics
+        ckpt.save(path)
+        return
+
+    # restore metrics
+    def restore_metrics(self, model, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="metrics-1")
+        ckpt = tf.train.Checkpoint()
+
+        # create initialized metrics
+        restored = []
+        for metric in model.metrics:
+            name = metric.name
+            if name == "loss":
+                name = "Mean"
+            name = "".join([w.capitalize() for w in  name.split("_")])
+            restored.append(tf.keras.metrics.get(name))
+
+        # restore metrics
+        ckpt.metrics = restored
+        ckpt.restore(path)
+
+        # merge states and update logs
+        logs = {}
+        for m1, m2 in zip(model.metrics, restored):
+            m1.merge_state([m2])
+            logs[m1.name] = m1.result()
+        return logs
+
+    # save a history
+    def save_history(self, callbacks, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="history.pkl")
+
+        # get a history object from callbacks
+        history = None
+        for callback in callbacks.callbacks:
+            if callback.__class__.__name__ == "History":
+                history = callback
+                break
+        joblib.dump({"epoch": history.epoch, "history": history.history}, path)
+        return
+
+    # restore a history
+    def restore_history(self, callbacks, epoch, step):
+        path = self.ckpt_path.format(epoch=epoch, step=step, name="history.pkl")
+        restored = joblib.load(path)
+
+        for callback in callbacks.callbacks:
+            if callback.__class__.__name__ == "History":
+                callback.epoch = restored["epoch"]
+                callback.history = restored["history"]
+                break
+        return
+    
+
+def train_model(data, model, epochs, initial_epoch=0, initial_step=0, epoch_period=0, step_period=0, ckpt_dir="checkpoint"):
+    cm = CheckpointManager(ckpt_dir)
+
+    iterator = iter(data)
+
+    # restore the dataset and the model weights before creating callbacks
+    if initial_epoch != 0 or initial_step != 0:
+        cm.restore_iterator(iterator, initial_epoch, initial_step)
+        cm.restore_model2(model, initial_epoch, initial_step)
+
+    steps = len(data)
+    callbacks = tf.keras.callbacks.CallbackList(add_history=True, add_progbar=True, model=model, epochs=epochs, steps=steps, verbose=True)
+
+    callbacks.on_train_begin()
+    train_fn = model.make_train_function()
+
+    # restore the hisotry after calling callbacks.on_train_begin()
+    if initial_epoch != 0 or initial_step != 0:
+        cm.restore_history(callbacks, initial_epoch, initial_step)
+
+    logs = None
+    for epoch in range(initial_epoch, epochs):
+
+        # initialize states 
+        if initial_step == 0:
+            iterator = iter(data)
+            model.reset_metrics()
+        
+        callbacks.on_epoch_begin(epoch)
+
+        for step in range(initial_step, steps):
+            callbacks.on_train_batch_begin(step)
+            logs = train_fn(iterator)
+
+            # restore the metrics and update the logs after calling the first train_fn()
+            if initial_step != 0 and step == initial_step:
+                logs = cm.restore_metrics(model, initial_epoch, initial_step)
+
+            callbacks.on_train_batch_end(step + 1, logs)
+            
+            # save information every "epochs_period" epochs
+            if step_period != 0 and (step + 1) % step_period == 0:
+                cm.save_iterator(iterator, epoch, step + 1)
+                cm.save_model(model, epoch, step + 1)
+                cm.save_metrics(model, epoch, step + 1)
+                cm.save_history(callbacks, epoch, step + 1)
+
+        # reset after the first epoch
+        if initial_step != 0:
+            initial_step = 0
+
+        callbacks.on_epoch_end(epoch, logs)
+
+        # save information every "epochs_period" epochs
+        if epoch_period != 0 and (epoch + 1) % epoch_period == 0:
+            cm.save_iterator(iterator, epoch + 1, 0)
+            cm.save_model(model, epoch + 1, 0)
+            cm.save_metrics(model, epoch + 1, 0)
+            cm.save_history(callbacks, epoch + 1, 0)
+
+    callbacks.on_train_end(logs=logs)
+    return model.history
